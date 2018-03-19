282a212eafff1219bef1cb1707aa2ee05d999414
krlim417 <krlim417@gmail.com>
2018-03-19 15:40:26 -0700

    Defines instance and extends methods onto the instance

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,11 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = {};
+  _(someInstance).extend(queueMethods);
+  someInstance._storage = {};
+  someInstance._storageSize = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
