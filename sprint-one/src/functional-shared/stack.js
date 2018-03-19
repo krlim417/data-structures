3eb0e73131de58d60259fe55b9a3abfd84644dd1
krlim417 <krlim417@gmail.com>
2018-03-19 16:50:46 -0700

    Adds push and size methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,12 +2,18 @@ const Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   let someInstance = {};
-  _(someInstance).extends(stackMethods);
+  _(someInstance).extend(stackMethods);
   someInstance._storage = {};
   someInstance._storageSize = 0;
   return someInstance;
 };
 
 const stackMethods = {};
+stackMethods.push = function(value) {
+  this._storage[++this._storageSize] = value;
+};
+stackMethods.size = function() {
+  return this._storageSize;
+};
 
 
