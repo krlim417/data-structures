f3353f1a866d08ef4479558a41f1918431c75e65
krlim417 <krlim417@gmail.com>
2018-03-19 16:55:28 -0700

    Creates instance object with delegation to stackMethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = Object.create(stackMethods);
+  someInstance._storage = {};
+  someInstance._storageSize = 0;
+  return someInstance;
 };
 
 var stackMethods = {};
