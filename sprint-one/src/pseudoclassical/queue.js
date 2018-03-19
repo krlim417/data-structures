c74e849c15ef9ff734142c0675d93587b971d02a
krlim417 <krlim417@gmail.com>
2018-03-19 16:17:19 -0700

    Adds properties and enqueue and size methods to object constructor

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,13 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._storageSize = 0;
 };
 
-
+Queue.prototype.enqueue = function(value) {
+  this._storage[++this._storageSize] = value;
+};
+Queue.prototype.size = function() {
+  return this._storageSize;
+};
