9effb569718bcd1b8b3a15e023a080270575f064
krlim417 <krlim417@gmail.com>
2018-03-19 16:36:55 -0700

    Implements push, pop, and size methods

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,27 @@
-var Stack = function() {
-  var someInstance = {};
+const Stack = function() {
+  const someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  let storage = {};
+  let storageSize = 0;
+
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[++storageSize] = value;
   };
 
   someInstance.pop = function() {
+    const popValue = storage[storageSize];
+    delete storage[storageSize];
+    if (storageSize > 0) {
+      storageSize--;
+    }
+    return popValue;
   };
 
   someInstance.size = function() {
+    return storageSize;
   };
 
   return someInstance;
