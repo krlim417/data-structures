31d866bf39c6617cc2a2e802ec9995d589de1029
krlim417 <krlim417@gmail.com>
2018-03-19 16:26:21 -0700

    Adds dequeue method to object constructor

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -8,6 +8,16 @@ var Queue = function() {
 Queue.prototype.enqueue = function(value) {
   this._storage[++this._storageSize] = value;
 };
+Queue.prototype.dequeue = function() {
+  const dequeueValue = this._storage[1];
+  for (let key in this._storage) {
+    this._storage[key] = this._storage[++key];
+  }
+  if (this._storageSize > 0) {
+    this._storageSize--;
+  }
+  return dequeueValue;
+};
 Queue.prototype.size = function() {
   return this._storageSize;
 };
