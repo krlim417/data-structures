69306614cde417983108e5e7936261a54329553c
krlim417 <krlim417@gmail.com>
2018-03-19 15:23:40 -0700

    Implements enqueue and size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,21 @@
-var Queue = function() {
-  var someInstance = {};
+const Queue = function() {
+  let someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  let storage = {};
+  let storageSize = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[++storageSize] = value;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return storageSize;
   };
 
   return someInstance;
