917a89496762c0ac4b9d322deaa45102a8471fbf
krlim417 <krlim417@gmail.com>
2018-03-19 17:01:12 -0700

    Adds properties and push and size methods to object constuctor

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,12 @@
-var Stack = function() {
+const Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._storageSize = 0;
 };
-
-
+Stack.prototype.push = function(value) {
+  this._storage[++this._storageSize] = value;
+};
+Stack.prototype.size = function() {
+  return this._storageSize;
+};
