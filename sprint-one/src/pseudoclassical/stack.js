a0a3dde43335917a05657f6fcfb45ad7bb911dde
krlim417 <krlim417@gmail.com>
2018-03-19 17:01:49 -0700

    Adds pop method to object constructor

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -7,6 +7,14 @@ const Stack = function() {
 Stack.prototype.push = function(value) {
   this._storage[++this._storageSize] = value;
 };
+Stack.prototype.pop = function() {
+  const popValue = this._storage[this._storageSize];
+  delete this._storage[this._storageSize];
+  if (this._storageSize > 0) {
+    this._storageSize--;
+  }
+  return popValue;
+};
 Stack.prototype.size = function() {
   return this._storageSize;
 };
