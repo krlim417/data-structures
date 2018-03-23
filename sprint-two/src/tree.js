cfaa0db8f207a25ea7e10e4a782f3cd1a00a1ed7
krlim417 <krlim417@gmail.com>
2018-03-23 15:15:07 -0700

    Implements contains method

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -16,6 +16,16 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children) {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
