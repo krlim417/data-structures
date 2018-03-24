fac9f27f49987bc6e222df0d95816157c9a739c6
krlim417 <krlim417@gmail.com>
2018-03-24 13:22:32 -0700

    Adds storage property, and implements addNode and contains methods

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,21 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this._storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this._storage[node] = this._storage[node] || { edges: {} };
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this._storage[node] !== undefined;
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(nodeToRemove) {
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
