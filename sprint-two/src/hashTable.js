8947f19dc49cc377c2cb98bc8bc050a6eb811725
krlim417 <krlim417@gmail.com>
2018-03-27 12:21:09 -0700

    Implements resize method, and halves or doubles size hash table size accordingly

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,6 @@
 const HashTable = function() {
   this._limit = 8;
+  this._size = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -15,6 +16,10 @@ HashTable.prototype.insert = function(k, v) {
   }
   bucket.push([k, v]);
   this._storage.set(index, bucket);
+  this._size++;
+  if (this._size >= this._limit * .75) {
+    this._resize(this._limit * 2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -37,6 +42,27 @@ HashTable.prototype.remove = function(k) {
     if (tuple[0] === k) {
       bucket.splice(i, 1);
     }
+  }
+  this._size--;
+  if (this._size < this._limit * .25) {
+    this._resize(this._limit / 2);
+  }
+};
+
+HashTable.prototype._resize = function(newLimit) {
+  if (newLimit >= 8 && newLimit !== this._limit) {
+    const oldStorage = this._storage;
+    this._limit = newLimit;
+    this._size = 0;
+    this._storage = LimitedArray(this._limit);
+    oldStorage.each(function(bucket) {
+      if (bucket) {
+        for (let i = 0; i < bucket.length; i++) {
+          const tuple = bucket[i];
+          this.insert(tuple[0], tuple[1]);
+        }
+      }
+    }.bind(this));
   }
 };
 
