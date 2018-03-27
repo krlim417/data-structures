d6c64c96438178e1a36961c2d1d947dcd74404bc
krlim417 <krlim417@gmail.com>
2018-03-27 10:53:32 -0700

    Creates instance object with delegation to binaryTreePrototype

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,21 @@
 var BinarySearchTree = function(value) {
+  const binaryTree = Object.create(binaryTreePrototype);
+  binaryTree.value = value;
+  binaryTree.left = null; 
+  binaryTree.right = null;
+  return binaryTree;
 };
 
+const binaryTreePrototype = {};
+
+binaryTreePrototype.insert = function(value) {
+};
+
+binaryTreePrototype.contains = function(value) {
+};
+
+binaryTreePrototype.depthFirstLog = function(callback) {
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
