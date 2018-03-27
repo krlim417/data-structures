c91ffd122b77464fd9dd5a1b934cc6fa47df5c6a
krlim417 <krlim417@gmail.com>
2018-03-27 09:40:24 -0700

    Implements insert method

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,26 @@
-
-
-var HashTable = function() {
+const HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  const index = getIndexBelowMaxForKey(k, this._limit);
+  const bucket = this._storage.get(index) || [];
+  for (let i = 0; i < bucket.length; i++) {
+    const tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      return;
+    }
+  }
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
 
